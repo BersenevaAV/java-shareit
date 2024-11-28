e1059656cd498eeec6029ce2260845ee0334a802
BersenevaAV <BersenevaAV@yandex.ru>
2024-11-28 04:38:54 +0300

    Методы тестирования Comments

--- server/src/test/java/ru/practicum/shareit/item/ItemServiceSpringBootTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemServiceSpringBootTest.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.booking.Booking;
+import ru.practicum.shareit.booking.BookingService;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.item.comment.Comment;
+import ru.practicum.shareit.item.comment.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemRequestDto;
 import ru.practicum.shareit.item.model.Item;
@@ -14,6 +19,7 @@ import ru.practicum.shareit.request.ItemRequestService;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
+import java.time.LocalDateTime;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,9 +33,12 @@ class ItemServiceSpringBootTest {
     private ItemRequestService itemRequestService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private BookingService bookingService;
     private ItemRequestDto itemRequestDto;
     private ItemRequest request;
     private User user;
+    private BookingDto bookingDto;
 
     @BeforeEach
     void setUp() {
@@ -42,6 +51,9 @@ class ItemServiceSpringBootTest {
         itemRequestDto.setName("item");
         itemRequestDto.setDescription("itemDesc");
         itemRequestDto.setAvailable(true);
+        bookingDto = new BookingDto();
+        bookingDto.setStart(LocalDateTime.now().minusDays(3));
+        bookingDto.setEnd(LocalDateTime.now().minusDays(1));
     }
 
     @Test
@@ -96,5 +108,21 @@ class ItemServiceSpringBootTest {
         itemRequestService.createRequest(newUser.getId(),request);
         ItemDto newItem = itemService.createItem(newUser.getId(),itemRequestDto);
         assertEquals(1,itemService.getAll(newUser.getId()).size());
+    }
+
+    @Test
+    void createComment() {
+        UserDto newUser1 = userService.createUser(user);
+        User user2 = new User();
+        user2.setName("user2");
+        user2.setEmail("us2@mail.ru");
+        UserDto newUser2 = userService.createUser(user);
+        ItemDto newItem = itemService.createItem(newUser1.getId(),itemRequestDto);
+        bookingDto.setItemId(newItem.getId());
+        bookingService.createBooking(newUser2.getId(),bookingDto);
+        Comment comment = new Comment();
+        comment.setText("Ok");
+        CommentDto newComment = itemService.createComment(newItem.getId(),newUser2.getId(),comment);
+        assertEquals("Ok",newComment.getText());
     }
 }
